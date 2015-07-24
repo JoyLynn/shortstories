1e3f0027f5b5d4e1ec55d184d4821cdaeeed3b21
wolfieorama <wachirawamunyi@gmail.com>
2015-07-24 19:53:01 +0300

    all stories java classes added

--- app/src/main/java/com/jjh/shortstories/story3.java
+++ app/src/main/java/com/jjh/shortstories/story3.java
@@ -1,7 +1,13 @@
-package com.jjh.shortstories;
+import com.jjh.shortstories.R;
 
-/**
- * Created by root on 7/24/15.
- */
-public class story3 {
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+public class story3 extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.story3);
+    }
 }
--- app/src/main/java/com/jjh/shortstories/story4.java
+++ app/src/main/java/com/jjh/shortstories/story4.java
@@ -1,7 +1,13 @@
 package com.jjh.shortstories;
 
-/**
- * Created by root on 7/24/15.
- */
-public class story4 {
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+public class story4 extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.story4);
+    }
 }
